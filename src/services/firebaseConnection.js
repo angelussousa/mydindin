aee05bbb6ed5010e79b27e40084d2ded7e7355af
angelusamadeus <angelus.sousa@hotmail.com>
2022-08-07 15:33:20 -0300

    editando o firebaseconnection para o github

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -5,13 +5,13 @@ import 'firebase/firebase-storage'
 
 
 let firebaseConfig = {
-  apiKey: "AIzaSyBZcZnS8Dxykf_A5yh_7pUq6hVQv6cs_Hk",
-  authDomain: "mydindin-69a3f.firebaseapp.com",
-  databaseURL: "https://mydindin-69a3f-default-rtdb.firebaseio.com",
-  projectId: "mydindin-69a3f",
-  storageBucket: "mydindin-69a3f.appspot.com",
-  messagingSenderId: "842689252382",
-  appId: "1:842689252382:web:b62b7160607dbe0f0cb20f"
+  apiKey: "XXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXX"
 };
 
   
